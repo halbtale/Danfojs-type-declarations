e5ffa34e9d1858bd8df4d76df902a540ffcd3989
Alberto <alberto.ice04@gmail.com>
2020-12-29 11:12:21 +0100

    Updated exports

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,14 @@
+import { Series } from "./core/series";
+import { DataFrame } from "./core/frame";
+import { to_datetime } from "./core/timeseries";
+import { merge } from "./core/merge";
+import { concat } from "./core/concat";
+import { date_range } from "./core/date_range";
+import { get_dummies } from "./core/get_dummies";
+import { read_csv, read_json, read_excel, read } from "./io/reader";
+import { LabelEncoder, OneHotEncoder } from "./preprocessing/encodings";
+import { MinMaxScaler, StandardScaler } from "./preprocessing/scalers";
+
 declare module 'danfojs' {
-    export { Series } from "./core/series";
-    export { DataFrame } from "./core/frame";
-    export { to_datetime } from "./core/timeseries";
-    export { merge } from "./core/merge";
-    export { concat } from "./core/concat";
-    export { date_range } from "./core/date_range";
-    export { get_dummies } from "./core/get_dummies";
-    export { read_csv, read_json, read_excel, read } from "./io/reader";
-    export { LabelEncoder, OneHotEncoder } from "./preprocessing/encodings";
-    export { MinMaxScaler, StandardScaler } from "./preprocessing/scalers";
+    export { Series, DataFrame, to_datetime, merge, concat, date_range, get_dummies, read_csv, read_json, read_excel, read, LabelEncoder, OneHotEncoder, MinMaxScaler, StandardScaler }
 }
